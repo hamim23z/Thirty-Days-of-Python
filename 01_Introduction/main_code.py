f183f6dccfcb6579893c70dbb54b874b47ec6ab4
Hamim <hamimc232@gmail.com>
2024-10-19 19:28:49 -0400

    Day 1 Notes and Exercises Completed

---
+++ 01_Introduction/main_code.py
@@ -0,0 +1,75 @@
+import math
+#Level One Exercises#
+
+#Number 1
+# Currently using Python version 3.12.3
+
+#Number 2
+# Complete
+
+#Number 3
+# Complete
+
+#Number 4
+# Complete
+
+
+
+#Level Two Exercises#
+
+#Number 1
+# Currently using Python version 3.12.3
+
+#Number 2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+#Number 3
+print("Hamim")
+print("Choudhury")
+print("USA")
+print("I am enjoying 30 Days of Python")
+
+
+#Number 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Hamim', 'Python', 'USA']))
+print(type("Hamim"))
+print(type("Choudhury"))
+print(type("USA"))
+
+
+
+#Level Three Exercises#
+#Number 1
+number = 2
+float = 9.81
+complex = 4-4j
+bool = True
+list = ['Hamim', 20, 'USA']
+tuple = ("Hello", "Hello", "Yes", 20)
+set = {20, 20, 23, 24, 25}
+dict = {
+    'firstname': "Hamim",
+    'lastname': "Choudhury",
+    'age': 20
+}
+
+#Number 2
+x1 = 2
+y1 = 3
+x2 = 10
+y2 = 8
+newx = (10-2)**2
+newy = (8-3)**2
+
+final_answer = math.sqrt(newx + newy)
+print(final_answer)
--- README.md
+++
@@ -1,1 +0,0 @@
-# Thirty-Days-of-Python
