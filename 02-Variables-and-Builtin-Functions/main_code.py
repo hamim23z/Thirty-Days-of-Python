e90cfb7c029dc20c5608be8b8f7a38d627c4dd4c
Hamim <hamimc232@gmail.com>
2024-11-11 17:34:19 -0500

    Day 2 of Python Exercises Complete

---
+++ 02-Variables-and-Builtin-Functions/main_code.py
@@ -0,0 +1,131 @@
+"""
+Level One Exercises are Below
+"""
+# Number One
+# Not creating a separate folder, all work will be done in this
+
+
+# Number Two
+# Day 2: 30 Days of python programming
+
+
+# Number Three
+first_name = "Hamim"
+
+
+# Number Four
+last_name = "Choudhury"
+
+
+# Number Five
+full_name = "Hamim Choudhury"
+
+
+# Number Six
+country = "USA"
+
+
+# Number Seven
+city = "New York City"
+
+
+# Number Eight
+age = 20
+
+
+# Number Nine
+year = 2024
+
+
+# Number Ten
+is_married = False
+
+
+# Number Eleven
+is_true = True
+
+
+# Number Twelve
+is_light_on = True
+
+
+# Number Thirteen
+weather, number, random = True, 100, "Hello"
+
+
+
+"""
+Level Two Exercises are Below
+"""
+# Number One
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_light_on))
+print(type(weather))
+
+
+# Number Two
+print(len(first_name))
+
+
+# Number Three
+if len(first_name) > len(last_name):
+    print("First name is longer than last name")
+else:
+    print("Last name is longer than first name")
+
+
+# Number Four
+num_one = 5
+num_two = 4
+
+
+# Number Five
+total = num_one + num_two
+
+
+# Number Six
+diff = num_one - num_two
+
+
+# Number Seven
+product = num_one * num_two
+
+
+# Number Eight
+division = num_one / num_two
+
+
+# Number Nine
+remainder = num_two % num_one
+
+
+# Number Ten
+exp = num_one ** num_two
+
+
+# Number Eleven
+floor_division = num_one // num_two
+
+
+# Number Twelve
+radius = 30
+area_of_circle = (3.14)(30**2)
+circum_of_circle = (2)(3.14)(30)
+
+
+# Number Thirteen
+your_first_name = input("Enter first name: ")
+your_last_name = input("Enter last name: ")
+your_country = input("Enter your country: ")
+your_age = input("Enter age: ")
+
+
+# Number Fourteen
+#Done
