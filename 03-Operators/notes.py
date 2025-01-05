c5d43570e67e213b66aed5f09d1b1dde998b71f8
Hamim <hamimc232@gmail.com>
2025-01-05 18:44:35 -0500

    Day 03 of Notes Completed

---
+++ 03-Operators/notes.py
@@ -0,0 +1,29 @@
+"""
+Booleans
+    • A boolean data type represents two values, True or False. The T and F must be capitalized.
+"""
+
+
+
+"""
+Operators
+    • There are many different types of operators in Python. The first being the assignment operator
+    which is the most common. Example: x = 10, name - "Hamim"
+
+    
+    • The second being arithmetic operators. Different type of arithmetic operators are +, -, *, /, %,
+    //, **. The last three are modulus, floor division, and exponentiation.
+
+    
+    • The third being comparison operators. Different type of comparison operators are ==, !=, >, >=,
+    <, <=. There are also some other comparison operators that we use. They are used as is, is not, in, 
+    not in. 
+        • is: returns True if both variables are the same object. 
+        • is not: returns True if both variables are not the same object. 
+        • in: returns True if the list contains a certain item, x in y. 
+        • not in: returns True if the list does not have a certain item, x not in y.
+
+    
+    • The fourth kind of operators are logical operators. They are and, or, not. and returns True if both expressions
+    are True. or returns True if one expression is True. not just flips the True or False. 
+"""
