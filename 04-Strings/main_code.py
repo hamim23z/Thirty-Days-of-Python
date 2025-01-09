d50830b9262de631b8e7d6eb74d5e02b942000f0
Hamim <hamimc232@gmail.com>
2025-01-09 14:51:12 -0500

    Completed Day 04 Exercises

--- 04-Strings/main_code.py
+++ 04-Strings/main_code.py
@@ -2,3 +2,163 @@
 Level One Exercises are Below: 
 """
 
+#Number One
+thirty = "Thirty"
+days = "Days"
+of = "Of"
+python = "Python"
+print(thirty, days, of, python)
+
+
+#Number Two
+coding = "Coding"
+for1 = "For"
+all = "All"
+print(coding, for1, all)
+
+
+#Number Three
+company = "Coding For All"
+
+
+#Number Four
+print(company)
+
+
+#Number Five
+print(len(company))
+
+
+#Number Six
+print(company.upper())
+
+
+#Number Seven
+print(company.lower())
+
+
+#Number Eight
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+
+#Number Nine
+first_word = company[0:6]
+print(first_word)
+
+
+#Number Ten
+print(company.find('Coding'))
+
+
+#Number Eleven
+print(company.replace("Coding", "Python"))
+
+
+#Number Twelve
+sentence = "Python for Everyone"
+print(sentence.replace("Everyone", "All"))
+
+
+#Number Thirteen
+print(company.split(" "))
+
+
+#Number Fourteen
+tech_companies = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(tech_companies.split(", "))
+
+
+#Number Fifteen
+first_char = company[0]
+print(first_char)
+
+
+#Number Sixteen
+last_char = company[-1]
+print(last_char)
+
+
+#Number Seventeen
+index10 = company[10]
+print(index10)
+
+
+#Number Eighteen
+acronym1 = "PFE"
+
+
+#Number Nineteen
+acronym2 = "CFA"
+
+
+#Number Twenty
+print(acronym2.index("C"))
+
+
+#Number Twenty One
+print(acronym2.index("F"))
+
+
+#Number Twenty Two
+sentence2 = "Coding For All People"
+print(sentence2.rfind("l"))
+
+
+#Number Twenty Three
+because = "You cannot end a sentence with because because because is a conjunction"
+print(because.index("because"))
+
+
+#Number Twenty Four
+because2 = "You cannot end a sentence with because because because is a conjunction"
+print(because2.rindex("because"))
+
+
+#Number Twenty Five
+slicingout = because[31:54]
+print(slicingout)
+
+
+#Number Twenty Six
+print(because.index("because"))
+
+
+#Number Twenty Seven
+slicingout2 = because[31:54]
+print(slicingout2)
+
+
+#Number Twenty Eight
+print(company.startswith("Coding"))
+
+
+#Number Twenty Nine
+print(company.endswith("coding"))
+
+
+#Number Thirty
+company_edited = '   Coding For All      '
+print(company_edited.strip(' '))
+
+
+#Number Thirty One
+#The second one with return True. 
+
+
+#Number Thirty Two (***)
+libraries = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+joined_string = "# ".join(libraries)
+
+
+#Number Thirty Three
+print("I am enjoying this challenge.\nI just wonder what is next.")
+
+
+#Number Thirty Four
+print("Name\tAge\tCountry\tCity\nHamim\t20\tUSA\tNYC")
+
+
+#Skipping Number Thirty Five and Thirty Six
+#Will do at a later time. 
